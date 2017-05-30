bec569c9338bf4fa5498192cadabb0095af6abc5
Cody Taylor <ctaylor4874@gmail.com>
2017-05-30 17:03:28 -0500

    Finished Parsers

--- lat_long_scraper/lat_long_scraper.py
+++ lat_long_scraper/lat_long_scraper.py
@@ -2,10 +2,9 @@
 Module holds the object models for the Happy Hour app
 """
 import os
-import pandas as pd
-import datetime
-import time
+import csv
 import requests
+import time
 import json
 import logging
 import argparse
@@ -33,80 +32,192 @@ class Base:
             f.write(response.content)
 
     def get_load(self, api_url, fname):
-        print(api_url)
         res = requests.get(api_url)
         self.write_response(res, fname)
-        if res.status_code == 400:
-            return dict({None: None})
+        self.check_api_response(api_url, res)
         str_response = res.content.decode('utf-8')
         return json.loads(str_response)
+
+    @staticmethod
+    def check_api_response(api_url, res):
+        if 'foursquare' in api_url:
+            if res.status_code == 200:
+                logging.info('Limit Remaining: {}'.format(res.headers.pop('X-RateLimit-Remaining')))
+            if res.status_code == 403:
+                logging.info('Rate Limit will Reset at: {}'.format(res.headers.pop('X-RateLimit-Reset')))
+                time.sleep(1825)  # delay for one hour once 5000 queries has been hit
+        if res.status_code == 400:
+            pass
 
 
 class GoogleDetails(Base):
     def __init__(self, place_id):
         super().__init__()
         self.place_id = place_id
-
-    @property
-    def google_api_data(self):
-        return self.get_load("https://maps.googleapis.com/maps/api/place/details/json?placeid={}&key={}".format(
+        self.data = self.get_load("https://maps.googleapis.com/maps/api/place/details/json?placeid={}&key={}".format(
             self.place_id, self.GOOGLE_API_KEY), 'GOOGLE_API_DATA.txt')
 
     @property
+    def result(self):
+        if 'result' in self.data:
+            return self.data['result']
+
+    @property
+    def address(self):
+        if 'formatted_address' in self.result:
+            return self.result['formatted_address']
+        pass
+
+    @property
+    def phone_number(self):
+        if 'formatted_phone_number' in self.result:
+            return self.result['formatted_phone_number']
+
+    @property
+    def opening_hours(self):
+        if 'opening_hours' in self.result:
+            return self.result['opening_hours']
+        return {}
+
+    @property
+    def hours(self):
+        if 'weekday_text' in self.opening_hours:
+            return self.opening_hours['weekday_text']
+        return {}
+
+    @property
+    def rating(self):
+        if 'rating' in self.result:
+            return self.result['rating']
+
+    @property
+    def geometry(self):
+        if 'geometry' in self.result:
+            return self.result['geometry']
+
+    @property
+    def location(self):
+        if 'location' in self.geometry:
+            return self.geometry['location']
+
+    @property
     def lat(self):
-        return str(self.google_api_data['result'].get('geometry').get('location').get('lat'))
+        if 'lat' in self.location:
+            return str(self.location['lat'])
 
     @property
     def lng(self):
-        return str(self.google_api_data['result'].get('geometry').get('location').get('lng'))
+        if 'lng' in self.location:
+            return str(self.location['lng'])
 
     @property
     def name(self):
-        return str(self.google_api_data.get('name'))
+        if 'name' in self.result:
+            return self.result['name']
+
+    def __repr__(self):
+        return "<Google Details: name: {}, lat: {}, lng: {}, rating: {}, hours: {}, phone_number: {}, address: {}>".format(
+            self.name, self.lat, self.lng, self.rating, self.hours, self.phone_number, self.address
+        )
 
 
-class FoursquareDetails(GoogleDetails):
+class FoursquareDetails(Base):
+    def __init__(self, google_details):
+        super().__init__()
+        self.lat = google_details.lat
+        self.lng = google_details.lng
+        self.name = google_details.name
+        self.foursquare_api_data = self.get_load(
+            "https://api.foursquare.com/v2/venues/search?intent=match&ll={},{}&query={}&client_id={}&client_secret={}&v=20170109".format(
+                str(self.lat), str(self.lng), self.name, self.FOURSQUARE_CLIENT_ID, self.FOURSQUARE_CLIENT_SECRET),
+            'FS_API_DATA.txt')
+
     @property
-    def foursquare_api_data_url(self):
-        return "https://api.foursquare.com/v2/venues/search?intent=match&ll={},{}&query={}&client_id={}&client_secret={}&v=20170109".format(
-            str(self.lat), str(self.lng), self.name, self.FOURSQUARE_CLIENT_ID, self.FOURSQUARE_CLIENT_SECRET)
+    def res(self):
+        if 'response' in self.foursquare_api_data:
+            return self.foursquare_api_data['response']
+
+    def has_menu(self):
+        if 'has_menu' in self.res:
+            return True
+        return False
+
+    def venues(self):
+        if 'venues' in self.res:
+            return self.res['venues']
 
     @property
-    def foursquare_api_venue_url(self):
-        return "https://api.foursquare.com/v2/venues/{}/menu?client_id={}&client_secret={}&v=20170109".format(
-            self.fs_venue_id, self.FOURSQUARE_CLIENT_ID, self.FOURSQUARE_CLIENT_SECRET)
-
-    def get_foursquare_venue_details(self):
-        return self.get_load(self.foursquare_api_venue_url, 'FS_VENUE_DETAILS.txt')
-
-    @property
-    def foursquare_venue_details(self):
-        return self.get_foursquare_venue_details()
+    def has_venues(self):
+        if 'venues' in self.res:
+            return bool(self.res['venues'])
 
     @property
     def fs_venue_id(self):
-        try:
-            return self.foursquare_api_data.get('venues')[0].get('id', '0')
-        except TypeError:
-            return None
+        if self.has_venues:
+            if 'id' in self.venues()[0]:
+                return self.venues()[0]['id']
+
+
+class FoursquareVenueDetails(FoursquareDetails, Base):
+    def __init__(self, google_details):
+        super().__init__(google_details)
+        self.foursquare_venue_details = self.get_load(
+            "https://api.foursquare.com/v2/venues/{}/menu?client_id={}&client_secret={}&v=20170109".format(
+                self.fs_venue_id, self.FOURSQUARE_CLIENT_ID, self.FOURSQUARE_CLIENT_SECRET), 'FS_VENUE_DETAILS.txt')
 
     @property
-    def foursquare_api_data(self):
-        return self.get_load(self.foursquare_api_data_url, 'FS_API_DATA.txt')
+    def res_detailed(self):
+        if 'response' in self.foursquare_venue_details:
+            return self.foursquare_venue_details['response']
+
+    @property
+    def menu(self):
+        if 'menu' in self.res_detailed:
+            return self.res_detailed['menu']
+        return {}
+
+    @property
+    def menus(self):
+        if 'menus' in self.menu:
+            return self.menu['menus']
+        return {}
+
+    @property
+    def menu_items(self):
+        if 'items' in self.menus:
+            return self.menus['items']
+        return {}
+
+    @staticmethod
+    def menu_name(menu):
+        if 'name' in menu:
+            return menu['name'].lower()
+        return ''
+
+    @staticmethod
+    def menu_description(menu):
+        if 'description' in menu:
+            return menu['description'].lower()
+        return ''
 
     @property
     def happy_hour_string(self):
-        for menu in self.foursquare_venue_details.get('menu').get('menus').get('items', [
-            {'name': '', 'description': ''}]):
-            if 'happy hour' in str(menu.get('name', '')).lower() or \
-                            'happy hour' in str(menu.get('description', '')).lower():
-                self.has_happy_hour = True
-                logging.debug(menu.get('description').lower())
-                return menu.get('description').lower()
-        pass
+        try:
+            for menu in self.menu_items:
+                if 'happy' in self.menu_name(menu) or 'happy' in self.menu_description(menu):
+                    self.has_happy_hour = True
+                    return self.menu_description(menu)
+        except AttributeError as e:
+            logging.info(e)
+            return None
+
+    def __repr__(self):
+        return "<FS Details: has_happy_hour: {}, happy_hour_string: {}, has_venues_list: {}, fs_venue_id: {}, >".format(
+            self.has_happy_hour, self.happy_hour_string, self.has_venues, self.fs_venue_id
+        )
 
 
-class GooglePlaces(GoogleDetails):
+class GooglePlaces(Base):
     def google_api_places_url(self, coordinates=None, radius=None):
         return "https://maps.googleapis.com/maps/api/place/radarsearch/json?location={}&radius={}&types=restaurant&key={}".format(
             coordinates, radius, self.GOOGLE_API_KEY)
@@ -116,52 +227,55 @@ class GooglePlaces(GoogleDetails):
 
     def get_place_info(self, coordinates, radius):
         for place in self.get_places_list(coordinates=coordinates, radius=radius):
-            self.place_id = place.get('place_id')
-            place_instance = FoursquareDetails(self.place_id)
-            print(place_instance.foursquare_api_data)
-            logging.debug(place_instance.fs_venue_id)
-            logging.debug(place_instance.google_api_data)
-            logging.debug(place_instance.foursquare_api_data)
-            if place_instance.fs_venue_id:
-                if place_instance.happy_hour_string:
-                    # self.db_connection.happyfinder.insert_one()
-                    print(place)
-
-    def __repr__(self):
-        return "place_id: {}, lat: {}, lng:{}, has_happy: {}, name: {}".format(self.place_id, self.lat,
-                                                                               self.lng,
-                                                                               self.has_happy_hour,
-                                                                               self.name)
+            place_id = place.get('place_id')
+            google_details = GoogleDetails(place_id)
+            fs = FoursquareDetails(google_details)
+            logging.info(google_details)
+            if fs.has_menu and fs.has_venues:
+                fs_detailed = FoursquareVenueDetails(google_details)
+                logging.info(fs_detailed)
+                if fs_detailed.happy_hour_string:
+                    data = dict({
+                        'name': google_details.name or None,
+                        'lat': google_details.lat or None,
+                        'lng': google_details.lng or None,
+                        'hours': google_details.hours or None,
+                        'rating': google_details.rating or None,
+                        'phone_number': google_details.phone_number or None,
+                        'address': google_details.address or None,
+                        'happy_hour_string': fs_detailed.happy_hour_string or None,
+                        'fs_venue_id': fs_detailed.fs_venue_id or None
+                    })
+                    file_exists = os.path.isfile(os.path.join(responses_dir, 'HAPPY_HOURS.csv'))
+                    with open(os.path.join(responses_dir, 'HAPPY_HOURS.csv'), 'a') as f:
+                        w = csv.DictWriter(f, data.keys())
+                        if not file_exists:
+                            w.writeheader()
+                        w.writerow(data)
+                    logging.info('ROW WRITTEN: {}'.format(data))
 
 
 def scrape():
-    ####Austin Scrape
-    # start here!!
-    # 30.254154,-97.73598
+    # Houston Scrape
+    # 29.590177, -95.556335
 
-    current_lat = 29.575846
-    current_lng = -95.594788
-    # current_lat = 29.738744
-    # current_lng = -95.465012
+    current_lat = 29.739638
+    current_lng = -95.446815
+
     # end at top right location
-    lat = 30.161752
-    lng = -95.202026
+    # Houston Scrape End
+    # 29.928755, -95.210266
+    lat = 29.743513
+    lng = -95.441666
 
     while current_lat < lat:
         while current_lng < lng:
-            # if querycount >= 3000:  # number of queries to 4square
-            #     print('pausing')
-            # querycount = 0
-            # time.sleep(1825)  # delay for one hour once 5000 queries has been hit
-            # loc.location = str(current_lat) + ',' + str(current_lng)
-            # GooglePlaces.get_place_info('{}{}'.format(lat, lng), '1610')
-            place = GooglePlaces(place_id=None)
+            place = GooglePlaces()
             place.get_place_info(coordinates='{},{}'.format(lat, lng), radius=1610)
             current_lng += 0.016635
-        current_lng = -95.594788
+        current_lng = -95.441666
         current_lat += 0.014466
-        # print
-        # "*****FINISHED*****"
+    logging.info('FINISHED')
 
 
 if __name__ == "__main__":

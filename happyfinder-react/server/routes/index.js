98f93f5462b46ffdc3a217a09ca4d487d249030d
Cody Taylor <ctaylor4874@gmail.com>
2017-05-31 21:59:20 -0500

    Setting up server env

---
+++ happyfinder-react/server/routes/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+import 'dotenv/config';
+import reactApp from './views/app';
+
+const routes = (app) => {
+  app.get('/api/records', require('./api/records'));
+  reactApp(app); // set up react routes
+};
+
+export default routes;
+/* eslint-enable global-require */

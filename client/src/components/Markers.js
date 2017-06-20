736ea44d328048e660fe91b86e32f3bfe724a80f
Cody Taylor <ctaylor4874@gmail.com>
2017-06-20 17:00:14 -0500

    Created function to map over venues array and create a unique marker for each item.

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -5,17 +5,26 @@ import React from 'react';
 
 import { Marker, Popup } from 'react-leaflet';
 
+function cleanHours(s){
+  // console.log(s)
+  s = s.split(', ', '\n');
+  return s;
+}
 export const makeMarkers = (venues) => (
   venues.map((venue) => {
     return (
-      <Marker position={[venue.lat, venue.lng]} id={venue.fs_venue_id}>
+      <Marker position={[venue.lat, venue.lng]} id={venue.fs_venue_id} riseOnHover={true} mouseOver={() => this.openPopup()}>
         <Popup>
           <span>
             <h3>{venue.name}</h3>
             <p><strong>Address: </strong>{venue.address}</p>
+            <p><strong>Website: </strong><a href={venue.url} target="_blank" rel="noopener noreferrer" >{venue.url}</a></p>
             <p><strong>Category: </strong>{venue.category}</p>
             <p><strong>Miles Away: </strong>{Math.round(Number(venue.milesfromuser) * 100) / 100}</p>
-            <p></p>
+            <p><strong>Hours: </strong>{cleanHours(venue.hours)}</p>
+            <p><strong>Phone Number: </strong>{venue.phone_number}</p>
+            <p><strong>Price: </strong>{venue.price} out of 5</p>
+            <p><strong>Rating: </strong>{venue.rating} out of 5</p>
           </span>
         </Popup>
       </Marker>

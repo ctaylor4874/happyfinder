fcf73fd515835cf7fc83c8440796c51687907f1c
Cody Taylor <ctaylor4874@gmail.com>
2017-06-20 17:00:47 -0500

    Added a search radius circle to visualise the radius searched in.

--- client/src/components/SearchRadius.js
+++ client/src/components/SearchRadius.js
@@ -1,3 +1,17 @@
 /**
  * Created by cody on 6/20/17.
  */
+import React from 'react';
+import {Circle} from 'react-leaflet';
+
+export const radius = (props) => {
+  return (
+    <Circle
+      center={[props.latLng.lat, props.latLng.lng]}
+      radius={props.radius * 1609.34}
+      fillOpacity={.1}
+      fillColor="red"
+      color="blue"
+    />
+  );
+};

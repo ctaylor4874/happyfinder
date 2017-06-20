1bbeed0eb2ddf266cf40c1d3ddd78d145cd6961d
Cody Taylor <ctaylor4874@gmail.com>
2017-06-20 16:56:22 -0500

    Added user info to state.

--- client/src/reducers/VenuesReducer.js
+++ client/src/reducers/VenuesReducer.js
@@ -3,12 +3,14 @@
  */
 import { GET_VENUES } from '../actions/index';
 
-const INITIAL_STATE = { venues: {} };
+const INITIAL_STATE = { venues: {}, userInfo: {} };
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case GET_VENUES:
-      return { ...state, venues: action.payload.data };
+      state = INITIAL_STATE;
+      let userInfo = action.payload.data.pop()[0];
+      return { ...state, venues: action.payload.data, userInfo };
     default:
       return state;
   }

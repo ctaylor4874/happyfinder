38d730b210cf3f0b1b29fda06d2bc272fb08664b
Cody Taylor <ctaylor4874@gmail.com>
2017-06-20 16:58:33 -0500

    Included make markers and make radius call.

--- client/src/containers/MapLocations.js
+++ client/src/containers/MapLocations.js
@@ -1,23 +1,34 @@
 import React, {Component} from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import {Redirect} from 'react-router-dom';
+import {Map, TileLayer} from 'react-leaflet';
 import {connect} from 'react-redux';
+
+import {getVenues} from '../actions/index';
+import {makeMarkers} from '../components/Markers';
+import {radius} from '../components/SearchRadius';
 
 class MapComponent extends Component {
   render() {
+    let userInfo = this.props.userInfo;
+    if (!(this.props.userInfo.latLng)) {
+      this.props.getVenues(this.props.match.params);
+      return(
+        <div className="container">
+          <h1 style={{color: 'white', marginLeft: '20', marginTop: '20'}}>Loading...</h1>
+        </div>
+      )
+    }
     return (
       <div className="col-xs-3">
-      <Map center={[33.85433699999997, -117.76149049999967]} zoom={13}>
-    <TileLayer
-      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    />
-    <Marker position={[33.85433699999997, -117.76149049999967]}>
-      <Popup>
-        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-      </Popup>
-    </Marker>
-  </Map>
-        </div>
+        <Map center={[userInfo.latLng.lat, userInfo.latLng.lng]} zoom={12}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {makeMarkers(this.props.venues)}
+          {radius(this.props.userInfo)}
+        </Map>
+      </div>
     );
   }
 }
@@ -26,8 +37,9 @@ function mapStateToProps(state) {
   return {
     errors: state.errors,
     isLoading: state.isLoading,
+    userInfo: state.venues.userInfo,
     venues: state.venues.venues,
   }
 }
 
-export default connect(mapStateToProps)(MapComponent)
+export default connect(mapStateToProps, {getVenues})(MapComponent)

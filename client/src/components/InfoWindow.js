8a78d4e1aff7af5e1256de63774444ac7427b949
Cody Taylor <ctaylor4874@gmail.com>
2017-06-21 17:02:10 -0500

    Created info window to contain details about each venue.

--- client/src/components/InfoWindow.js
+++ client/src/components/InfoWindow.js
@@ -1,3 +1,23 @@
 /**
  * Created by cody on 6/21/17.
  */
+import React from 'react';
+
+export const infoWindow = (venues, refs) => (
+  venues.map((venue) => {
+    return (
+      <div>
+        <h3><a href="#" onClick={(e) => {
+          const {fs_venue_id} = venue;
+          e.preventDefault();
+          // refs.forEach((ref) => {
+          //   if (ref === fs_venue_id){
+          //     ref.click();
+          //   }
+          })
+        }}>{venue.name}</a></h3>
+        <p>{venue.category}<span style={{float: 'right'}}>{Math.round(Number(venue.milesfromuser) * 100) / 100} miles</span></p>
+      </div>
+    )
+  })
+);

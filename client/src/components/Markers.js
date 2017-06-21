73efea6f177de8cbd614f48718f08b30914e8389
Cody Taylor <ctaylor4874@gmail.com>
2017-06-21 17:02:33 -0500

    Modified markers.

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -3,9 +3,9 @@
  */
 import React from 'react';
 
-import { Marker, Popup } from 'react-leaflet';
+import {Marker, Popup} from 'react-leaflet';
 
-function cleanHours(s){
+function cleanHours(s) {
   // console.log(s)
   s = s.split(', ', '\n');
   return s;
@@ -13,12 +13,19 @@ function cleanHours(s){
 export const makeMarkers = (venues) => (
   venues.map((venue) => {
     return (
-      <Marker position={[venue.lat, venue.lng]} id={venue.fs_venue_id} riseOnHover={true} mouseOver={() => this.openPopup()}>
+      <Marker
+        position={[venue.lat, venue.lng]}
+        id={venue.fs_venue_id}
+        riseOnHover={true}
+        ref={venue.fs_venue_id}
+        // onClick={(e) => e.target.openPopup()}
+      >
         <Popup>
           <span>
             <h3>{venue.name}</h3>
             <p><strong>Address: </strong>{venue.address}</p>
-            <p><strong>Website: </strong><a href={venue.url} target="_blank" rel="noopener noreferrer" >{venue.url}</a></p>
+            <p><strong>Website: </strong><a href={venue.url} target="_blank"
+                                            rel="noopener noreferrer">{venue.url}</a></p>
             <p><strong>Category: </strong>{venue.category}</p>
             <p><strong>Miles Away: </strong>{Math.round(Number(venue.milesfromuser) * 100) / 100}</p>
             <p><strong>Hours: </strong>{cleanHours(venue.hours)}</p>

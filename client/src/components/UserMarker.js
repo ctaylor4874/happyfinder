32dcae330d16fe8f65e12836adc797649b234647
Cody Taylor <ctaylor4874@gmail.com>
2017-06-21 17:03:32 -0500

    Created a user marker at the location the client requested.

--- client/src/components/UserMarker.js
+++ client/src/components/UserMarker.js
@@ -1,3 +1,15 @@
 /**
  * Created by cody on 6/21/17.
  */
+import React from 'react';
+import {CircleMarker} from 'react-leaflet';
+
+export const userMarker = (userInfo) => (
+  <CircleMarker
+    center={[userInfo.latLng.lat, userInfo.latLng.lng]}
+    color="red"
+    radius={20}
+    fillColor="red"
+  >
+  </CircleMarker>
+);

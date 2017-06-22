a2ab5559deb2f494cda4ba6299dd2911cfbaf731
Cody Taylor <ctaylor4874@gmail.com>
2017-06-22 17:05:38 -0500

    worked on infowindow for venues.

--- client/src/components/InfoWindow.js
+++ client/src/components/InfoWindow.js
@@ -3,15 +3,12 @@
  */
 import React from 'react';
 
-const clickMarker = (refs, id) => {
-  console.log(this.refs);
-};
-export const infoWindow = ({venues}) => (
+export const infoWindow = (venues) => (
   venues.map((venue) => {
     return (
-      <div>
-        <h3><a href="#" id={venue.fs_venue_id} onClick={(e) => clickMarker(e.target.id)}>{venue.name}</a></h3>
-        <p>{venue.category}<span style={{float: 'right'}}>{Math.round(Number(venue.milesfromuser) * 100) / 100} miles</span></p>
+      <div className="info-window">
+        <h4><a className="info-link" href="#" id={venue.fs_venue_id}>{venue.name}</a></h4>
+        <p>{venue.category}<span className="info-span">{Math.round(Number(venue.milesfromuser) * 100) / 100} miles</span></p>
       </div>
     )
   })

8855752d4b4ec214dfd8966c30620d83eb59e53d
Cody Taylor <ctaylor4874@gmail.com>
2017-06-23 15:27:51 -0500

    Added input element styling.

--- client/src/components/style.js
+++ client/src/components/style.js
@@ -7,10 +7,23 @@ export const style = {
   fontWeight: 600,
   color: 'rgb(0, 188, 212)',
 };
-
+export const inputStyle = {
+  textAlign: 'left',
+  fontSize: 25,
+  textShadow: '0 0 4px black',
+  fontWeight: 600,
+  color: 'lightgray',
+};
 export const selectStyle = {
   textAlign: 'left',
   fontSize: 25,
   fontWeight: 600,
   color: 'rgb(0, 188, 212)',
+};
+export const emailStyle = {
+  textAlign: 'left',
+  fontSize: 20,
+  textShadow: '0 0 4px black',
+  fontWeight: 600,
+  color: 'lightgray',
 };

779e3a1cc6c80d2ab3a2bdc7ef49a064560330e4
Cody Taylor <ctaylor4874@gmail.com>
2017-06-23 17:01:46 -0500

    Made each venue link to the details page for that venue.

--- client/src/components/InfoWindow.js
+++ client/src/components/InfoWindow.js
@@ -2,13 +2,23 @@
  * Created by cody on 6/21/17.
  */
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const infoWindow = (venues) => (
   venues.map((venue) => {
     return (
-      <div className="info-window">
-        <h4><a className="info-link" href="#" id={venue.fs_venue_id}>{venue.name}</a></h4>
-        <p>{venue.category}<span className="info-span">{Math.round(Number(venue.milesfromuser) * 100) / 100} miles</span></p>
+      <div className="info-window" key={venue.id_}>
+        <h4>
+          <Link
+            to={`/happyhours/venue/${venue.id_}`}
+            className="info-link"
+            id={venue.id_}
+            key={venue.id_}>
+            {venue.name}
+          </Link>
+        </h4>
+        <p>{venue.category}<span className="info-span">{Math.round(Number(venue.milesfromuser) * 100) / 100}
+          miles</span></p>
       </div>
     )
   })

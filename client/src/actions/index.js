1e95d8f600f2b7eaf6c3511038d262f121ebe4f5
Cody Taylor <ctaylor4874@gmail.com>
2017-06-23 17:00:37 -0500

    Added venue details http request.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,7 @@ export const GET_VENUES = 'GET_VENUES';
 export const ERROR = 'ERROR';
 export const IS_LOADING = 'IS_LOADING';
 export const SENT_EMAIL = 'SENT_EMAIL';
+export const VENUE_DATA = 'VENUE_DATA';
 
 function getAPIData(props) {
   const url = `/api/${props.userLocation}/${props.radius}`;
@@ -30,6 +31,22 @@ export function sendEmail(props){
   return dispatch => sendEmailData(props).then(
     (response) => {
       dispatch({type: SENT_EMAIL, payload: response})
+    },
+    (error) => {
+      dispatch({type: ERROR, payload: error.response});
+    },
+  )
+}
+
+function _getVenueData(id_){
+  const url = `/venue-data`;
+  return axios.post(url, {id_})
+}
+
+export function getVenueData(id_){
+  return dispatch => _getVenueData(id_).then(
+    (response) => {
+      dispatch({type: VENUE_DATA, payload: response})
     },
     (error) => {
       dispatch({type: ERROR, payload: error.response});

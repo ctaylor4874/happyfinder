a51f12cdc3063e9c41560301ff6b418862884c69
Cody Taylor <ctaylor4874@gmail.com>
2017-06-23 15:11:39 -0500

    Refactored imports, refactored app routes, added email sending route and processing.

--- server.js
+++ server.js
@@ -1,78 +1,34 @@
 /**
  * Created by cody on 6/2/17.
  */
-const mysql = require('mysql');
-const express = require("express");
-const axios = require('axios');
+import express from 'express';
+import bodyParser from 'body-parser';
 
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-const host = process.env.HAPPYFINDER_HOST;
-const user = process.env.HAPPYFINDER_USER;
-const password = process.env.HAPPYFINDER_PASSWORD;
-const database = process.env.HAPPYFINDER_DATABASE;
+import {handleDatabase} from './db';
+import {makeURL, getData, venuesQuery} from './routeHandling';
+import {getMailOptions, transport} from './emailHandling';
 
 const app = express();
 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.set('port', (process.env.PORT || 3001));
-
-const pool = mysql.createPool({
-  connectionLimit: 100,
-  host,
-  user,
-  password,
-  database,
-  debug: true
-});
-
-function handle_database(userInfo, req, res) {
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      connection.release();
-      res.json({"code": 100, "status": "Error in connection database"});
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-
-    connection.query(req, function (err, rows) {
-      connection.release();
-      if (!err) {
-        rows.push(userInfo);
-        res.json(rows);
-      } else {
-        console.log(err);
-      }
-    });
-
-    connection.on('error', function (err) {
-      res.json({"code": 100, "status": "Error in connection database"});
-    });
-  });
-}
-
-const query = (userLat, userLng, radius) => (
-  `SELECT *, 
-  (acos(sin(lat * 0.0175) * sin(${userLat} * 0.0175) + cos(lat * 0.0175) * 
-  cos(${userLat} * 0.0175) * cos((${userLng} * 0.0175) - (lng * 0.0175))) * 3959) 
-  AS milesfromuser 
-  FROM happyfinder 
-  WHERE (acos(sin(lat * 0.0175) * sin(${userLat} * 0.0175) + cos(lat * 0.0175) * cos(${userLat} * 0.0175) * 
-  cos((${userLng} * 0.0175) - (lng * 0.0175))) * 3959 <= ${radius}) 
-  ORDER BY milesfromuser ASC 
-  LIMIT 75;`
-);
 
 app.get("/api/:userLocation/:radius", function (req, res) {
   let userInfo = [];
   let userLocation = req.params.userLocation;
   let radius = req.params.radius || 5;
 
-  getUserLatLng(userLocation)
+  const url = makeURL(userLocation);
+  getData(url)
     .then(
       (response) => {
         let latLng = response.data.results[0].geometry.location;
         userInfo.push({latLng, userLocation, radius});
-        handle_database(userInfo, query(latLng.lat, latLng.lng, radius), res)
+        handleDatabase(userInfo, venuesQuery(latLng.lat, latLng.lng, radius), res)
       },
       (error) => {
         console.log("There was an error getting the lat/lng of the user.");
@@ -81,18 +37,14 @@ app.get("/api/:userLocation/:radius", function (req, res) {
     );
 });
 
-const getData = (url) => {
-  axios.get(url);
-  return axios.get(url);
-};
-const makeURL = (userLocation) => {
-  return `https://maps.googleapis.com/maps/api/geocode/json?address=${userLocation}&key=${GOOGLE_API_KEY}`
-};
+app.post("/send-mail", (req, res) => {
+  const fromEmail = req.body.fromEmail,
+    subject = req.body.subject,
+    message = req.body.message;
 
-const getUserLatLng = (userLocation) => {
-  const url = makeURL(userLocation);
-  return getData(url);
-};
+  const mailOptions = getMailOptions(fromEmail, subject, message);
+  transport(mailOptions, res);
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);

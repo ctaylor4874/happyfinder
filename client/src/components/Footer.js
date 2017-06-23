257f9238c10d70c7761c5d0c1000bcfadbc225b0
Cody Taylor <ctaylor4874@gmail.com>
2017-06-23 17:01:22 -0500

    Made changes to links.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,51 +1,59 @@
 /**
  * Created by cody on 6/22/17.
  */
-import React, {Component} from 'react';
-import FontIcon from 'material-ui/FontIcon';
+import React from 'react';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 import Code from 'material-ui/svg-icons/action/code';
+import Person from 'material-ui/svg-icons/social/person';
+import Details from 'material-ui/svg-icons/image/details';
 
 const codeIcon = <Code/>;
-const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
-const nearbyIcon = <IconLocationOn />;
+const personIcon = <Person />;
+const detailsIcon = <Details />;
 
-class Footer extends Component {
-  state = {
-    selectedIndex: 0,
-  };
-
-  select = (index) => this.setState({selectedIndex: index});
-
-  render() {
-    return (
+export const Footer = () => (
       <Paper zDepth={1} className="footer">
-        <BottomNavigation selectedIndex={this.state.selectedIndex}>
-          <a id="contact-links" href="https://github.com/ctaylor4874" target="_blank">
-            <BottomNavigationItem
-              label="Visit My Github"
-              icon={codeIcon}
-            />
-          </a>
-          <a href="mailto:cody.taylor@codybtaylor.com" id="contact-links">
-            <BottomNavigationItem
-              label="Favorites"
-              icon={favoritesIcon}
-              onTouchTap={() => this.select(1)}
-            />
-          </a>
-          <BottomNavigationItem
-            label="Nearby"
-            icon={nearbyIcon}
-            onTouchTap={() => this.select(2)}
-          />
+        <BottomNavigation>
+          <div className="col-xs-4">
+            <a
+              id="contact-links"
+              href="https://github.com/ctaylor4874"
+              target="_blank"
+              rel="noopener noreferrer">
+              <BottomNavigationItem
+                label="Visit My Github"
+                icon={codeIcon}
+              />
+            </a>
+          </div>
+          <div className="col-xs-4">
+            <a
+              id="contact-links"
+              href="https://www.linkedin.com/in/cody-taylor"
+              target="_blank"
+              rel="noopener noreferrer">
+              <BottomNavigationItem
+                label="Visit My LinkedIn"
+                icon={personIcon}
+              />
+            </a>
+          </div>
+          <div className="col-xs-4">
+            <a
+              id="contact-links"
+              href="https://github.com/ctaylor4874/happyfinder"
+              target="_blank"
+              rel="noopener noreferrer">
+              <BottomNavigationItem
+                label="View Project Details"
+                icon={detailsIcon}
+              />
+            </a>
+          </div>
         </BottomNavigation>
         <p>2017 &copy; All Rights Reserved.</p>
       </Paper>
     );
-  }
-}
 
 export default Footer;

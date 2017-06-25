f3ab5ae4433b611889439c249947266624d8b41d
Cody Taylor <ctaylor4874@gmail.com>
2017-06-24 23:08:14 -0500

    Created a loading screen component.

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,3 +1,14 @@
 /**
  * Created by codytaylor on 6/24/17.
  */
+import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+import {circleProgressStyle} from '../styles/style';
+
+const Loading = () => (
+  <div className="container">
+    <CircularProgress style={circleProgressStyle} size={80} thickness={7} />
+  </div>
+);
+
+export default Loading;

b1bb51ec65b47414063122b2890bc0bb58f72ab9
Cody Taylor <ctaylor4874@gmail.com>
2017-06-24 23:08:44 -0500

    Took some data off of the marker popups.

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -2,14 +2,7 @@
  * Created by cody on 6/20/17.
  */
 import React from 'react';
-
 import {Marker, Popup} from 'react-leaflet';
-
-function cleanHours(s) {
-  // console.log(s)
-  s = s.split(', ', '\n');
-  return s;
-}
 
 export const makeMarkers = (venues) => (
   venues.map((venue) => {
@@ -22,15 +15,12 @@ export const makeMarkers = (venues) => (
         <Popup>
           <span>
             <h3>{venue.name}</h3>
-            <p><strong>Address: </strong>{venue.address}</p>
-            <p><strong>Website: </strong><a href={venue.url} target="_blank"
-                                            rel="noopener noreferrer">{venue.url}</a></p>
             <p><strong>Category: </strong>{venue.category}</p>
             <p><strong>Miles Away: </strong>{Math.round(Number(venue.milesfromuser) * 100) / 100}</p>
-            <p><strong>Hours: </strong>{cleanHours(venue.hours)}</p>
-            <p><strong>Phone Number: </strong>{venue.phone_number}</p>
-            <p><strong>Price: </strong>{venue.price} out of 5</p>
-            <p><strong>Rating: </strong>{venue.rating} out of 5</p>
+            <p><strong>Happy Hour Details:</strong><br/>{(venue.happy_hour_string === 'Not Available') ?
+              'Details Not Available' :
+              venue.happy_hour_string.toUpperCase()}</p>
+            {/*<p><ul id="hours-list">{cleanHours(venue.hours)}</ul></p>*/}
           </span>
         </Popup>
       </Marker>

14258d7a9e37944701a18730acbe8e67dfe037e4
Cody Taylor <ctaylor4874@gmail.com>
2017-06-26 16:50:18 -0500

    Worked on link to details page in popup.

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -2,7 +2,7 @@
  * Created by cody on 6/20/17.
  */
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import {Marker, Popup} from 'react-leaflet';
 
 export const makeMarkers = (venues) => (
@@ -15,13 +15,8 @@ export const makeMarkers = (venues) => (
       >
         <Popup>
           <span>
-            <Link
-              to={`/happyhours/venue/${venue.id_}`}
-              className="info-link"
-              id={venue.id_}
-              key={venue.id_}>
-            {venue.name}
-          </Link>
+            {/*<span onClick={}><h3>{venue.name}</h3></span>*/}
+            <h3>{venue.name}</h3>
             <p><strong>Category: </strong>{venue.category}</p>
             <p><strong>Miles Away: </strong>{Math.round(Number(venue.milesfromuser) * 100) / 100}</p>
             <p><strong>Happy Hour Details:</strong><br/>{(venue.happy_hour_string === 'Not Available') ?

f247a5bb8dc6bb7fe249f3e45f87c7517345f85c
Cody Taylor <ctaylor4874@gmail.com>
2017-06-27 15:37:11 -0500

    Added query parameter escapes for sql injection and handler to check if in production mode to serve client/build.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 /**
  * Created by cody on 6/2/17.
  */
+require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
+import {escape} from 'mysql';
 
 import {handleDatabase} from './db';
 import {makeURL, getData, venuesQuery, venueQuery} from './dataHandling';
@@ -15,20 +17,25 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.set('port', (process.env.PORT || 3001));
+
 
 app.get("/api/:userLocation/:radius", function (req, res) {
   let userInfo = [];
   let userLocation = req.params.userLocation;
   let radius = req.params.radius || 5;
-
   const url = makeURL(userLocation);
+
   getData(url)
     .then(
       (response) => {
         let latLng = response.data.results[0].geometry.location;
         userInfo.push({latLng, userLocation, radius});
-        handleDatabase(userInfo, venuesQuery(latLng.lat, latLng.lng, radius), res)
+        handleDatabase(userInfo, venuesQuery(escape(latLng.lat), escape(latLng.lng), escape(radius)), res)
       },
       (error) => {
         console.log("There was an error getting the lat/lng of the user.");
@@ -38,18 +45,18 @@ app.get("/api/:userLocation/:radius", function (req, res) {
 });
 
 app.post("/send-mail", (req, res) => {
-  const fromEmail = req.body.fromEmail,
-    subject = req.body.subject,
-    message = req.body.message,
-    city = req.body.city,
-    state = req.body.state;
-
+  const fromEmail = escape(req.body.fromEmail),
+    subject = escape(req.body.subject),
+    message = escape(req.body.message),
+    city = escape(req.body.city),
+    state = escape(req.body.state);
   const mailOptions = getMailOptions(fromEmail, subject, message, city, state);
+
   transport(mailOptions, res);
 });
 
 app.post("/venue-data", (req, res) => {
-  const id_ = req.body.id_;
+  const id_ = escape(req.body.id_);
   handleDatabase(null, venueQuery(id_), res);
 });
 

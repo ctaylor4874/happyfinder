fc2c22351ef3b8a5aba1331089da7fe631c90f04
Cody Taylor <ctaylor4874@gmail.com>
2017-06-27 15:32:31 -0500

    Took out unecessary code.

--- client/src/components/Markers.js
+++ client/src/components/Markers.js
@@ -2,7 +2,6 @@
  * Created by cody on 6/20/17.
  */
 import React from 'react';
-import {Redirect} from 'react-router-dom';
 import {Marker, Popup} from 'react-leaflet';
 
 export const makeMarkers = (venues) => (
@@ -15,17 +14,15 @@ export const makeMarkers = (venues) => (
       >
         <Popup>
           <span>
-            {/*<span onClick={}><h3>{venue.name}</h3></span>*/}
             <h3>{venue.name}</h3>
             <p><strong>Category: </strong>{venue.category}</p>
             <p><strong>Miles Away: </strong>{Math.round(Number(venue.milesfromuser) * 100) / 100}</p>
             <p><strong>Happy Hour Details:</strong><br/>{(venue.happy_hour_string === 'Not Available') ?
               'Details Not Available' :
               venue.happy_hour_string.toUpperCase()}</p>
-            {/*<p><ul id="hours-list">{cleanHours(venue.hours)}</ul></p>*/}
           </span>
         </Popup>
       </Marker>
     )
   })
-)
+);

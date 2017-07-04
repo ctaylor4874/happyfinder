6c0f079c9ffcfb5e4dc4e03beea4c7ef96c882b1
Cody Taylor <ctaylor4874@gmail.com>
2017-07-03 17:24:38 -1000

    Debugging

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ import {getMailOptions, transport} from './emailHandling';
 
 const app = express();
 
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true

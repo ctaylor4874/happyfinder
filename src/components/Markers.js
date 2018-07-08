361cccb589065efbd4058531179bdcc754b1a108
Cody Taylor <ctaylor4874@gmail.com>
2018-07-08 16:32:12 -0500

    Changed marker to have link to venue

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -2,25 +2,15 @@
  * Created by cody on 6/20/17.
  */
 import React from "react";
-import PropTypes from 'prop-types'
 import { Marker, Popup } from "react-leaflet";
 
-class MakeMarkers extends React.Component {
-  static propTypes = {
-    venues: PropTypes.arrayOf(PropTypes.any).isRequired
-  }
-
-  goToVenue(id){
-    this.context.router.history.push(`/happyhours/venue/${id}`);
-  }
-
-  render() {
-    return this.props.venues.map(venue => {
-      return (
-        <Marker position={[venue.lat, venue.lng]} riseOnHover key={venue.id_}>
-          <Popup>
+const makeMarkers = venues =>
+  venues.map(venue => {
+    return (
+      <Marker position={[venue.lat, venue.lng]} riseOnHover key={venue.id_}>
+        <Popup>
           <span>
-            <h3 onClick={() => this.goToVenue(venue.id_)}>
+            <h3>
               {venue.name}
             </h3>
             <p>
@@ -39,11 +29,9 @@ class MakeMarkers extends React.Component {
                 : venue.happy_hour_string.toUpperCase()}
             </p>
           </span>
-          </Popup>
-        </Marker>
-      );
-    });
-  }
-}
+        </Popup>
+      </Marker>
+    );
+  });
 
-export default MakeMarkers;
+export default makeMarkers;
